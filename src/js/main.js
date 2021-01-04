030ed4d4f51a52495eb17a7976d780a602f90a43
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-01-04 10:46:35 +0300

    Init

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$('#slider').slick({
+	dots: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true,
+	autoplay: false,
+	arrows: true,
+	draggable: false,
+	// accessibility: false,
+	prevArrow: '#slide-prev',
+	nextArrow: '#slide-next',
+	// dotsClass: 'slider-dots'
+});
